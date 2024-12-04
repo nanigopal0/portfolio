e529e18518f8d98fabdb86eb30c6707c7824acab
Nanigopal Rana <nanigopalr972@gmail.com>
2024-12-04 20:15:19 +0530

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <header >
+        This is header
+      </header>
+      
+        This is body
+      
+      <footer>
+        This is footer
+      </footer>
+    </>
+  )
+}
+
+export default App
