2e6c443b02fd6b74fcba357645221c64616a05dd
Nanigopal Rana <nanigopalr972@gmail.com>
2025-12-08 20:14:26 +0530

    upgraded react version to 19

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [tailwindcss(), react()]
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    server: {
+      proxy: {
+        "/api": {
+          target: env.VITE_SERVER_URL || "http://localhost:8080",
+          changeOrigin: true,
+        },
+      },
+    },
+    plugins: [tailwindcss(), react()],
+  };
 });
